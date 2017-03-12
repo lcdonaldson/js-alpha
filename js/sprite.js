ac389ddaedf14e6cd8b106174f594277fcbed6d9
Levi Donaldson <lcdbacanas@gmail.com>
2017-03-11 23:53:10 -0600

    comment out unecessary

--- js/sprite.js
+++ js/sprite.js
@@ -56,47 +56,47 @@ var Sprite = function(fn) {
     };
 
 
-    this.rotAnim = function(x, y, sequence, angle)
-    {
-        if (AnimationCounter[AnimationCounterIndex].animationDelay++ >= 3) {
-            AnimationCounter[AnimationCounterIndex].animationDelay = 0;
-            AnimationCounter[AnimationCounterIndex].animationIndexCounter++;
-            if (AnimationCounter[AnimationCounterIndex].animationIndexCounter >= sequence.length)
-                AnimationCounter[AnimationCounterIndex].animationIndexCounter = 0;
-            AnimationCounter[AnimationCounterIndex].animationCurrentFrame = sequence[AnimationCounter[AnimationCounterIndex].animationIndexCounter];
-        }
-        var res = i2xy(AnimationCounter[AnimationCounterIndex].animationCurrentFrame, 8);
+    // this.rotAnim = function(x, y, sequence, angle)
+    // {
+    //     if (AnimationCounter[AnimationCounterIndex].animationDelay++ >= 3) {
+    //         AnimationCounter[AnimationCounterIndex].animationDelay = 0;
+    //         AnimationCounter[AnimationCounterIndex].animationIndexCounter++;
+    //         if (AnimationCounter[AnimationCounterIndex].animationIndexCounter >= sequence.length)
+    //             AnimationCounter[AnimationCounterIndex].animationIndexCounter = 0;
+    //         AnimationCounter[AnimationCounterIndex].animationCurrentFrame = sequence[AnimationCounter[AnimationCounterIndex].animationIndexCounter];
+    //     }
+    //     var res = i2xy(AnimationCounter[AnimationCounterIndex].animationCurrentFrame, 8);
 
-        Context.context.save();
-        Context.context.translate(x+16, y+16);    // Translate sprite to its center
-        Context.context.rotate(angle * this.TO_RADIANS);    // Rotate sprite around its center
-        Context.context.drawImage(this.image, res[0]*32, res[1]*32, 32, 32,
-            -16, -16,                         // Translate sprite back to its original position
-            32, 32);
-        Context.context.restore();
+    //     Context.context.save();
+    //     Context.context.translate(x+16, y+16);    // Translate sprite to its center
+    //     Context.context.rotate(angle * this.TO_RADIANS);    // Rotate sprite around its center
+    //     Context.context.drawImage(this.image, res[0]*32, res[1]*32, 32, 32,
+    //         -16, -16,                         // Translate sprite back to its original position
+    //         32, 32);
+    //     Context.context.restore();
 
-        AnimationCounterIndex++;
-    };
+    //     AnimationCounterIndex++;
+    // };
 
     // Stretched draw
-    this.draw2 = function(x, y, w, h) {
-        if (this.is_pattern) {
-            //Context.context.fillStyle = Context.context.createPattern(this.image, 'repeat');;
-            //Context.context.fillRect(x, y, w, h);
-            for (var i = 0; i < this.pattern_x_times; i++) {
-                Context.context.drawImage(this.image, x + w*i, y, w, h);
-            }
-        } else {
-            Context.context.drawImage(this.image, x, y, w, h);
-        }
-    };
+    // this.draw2 = function(x, y, w, h) {
+    //     if (this.is_pattern) {
+    //         //Context.context.fillStyle = Context.context.createPattern(this.image, 'repeat');;
+    //         //Context.context.fillRect(x, y, w, h);
+    //         for (var i = 0; i < this.pattern_x_times; i++) {
+    //             Context.context.drawImage(this.image, x + w*i, y, w, h);
+    //         }
+    //     } else {
+    //         Context.context.drawImage(this.image, x, y, w, h);
+    //     }
+    // };
 
-    // Rotated draw
-    this.rot = function(x, y, angle) {
-        Context.context.save();
-        Context.context.translate(x, y);
-        Context.context.rotate(angle * this.TO_RADIANS);
-        Context.context.drawImage(this.image, -(this.image.width/2), -(this.image.height/2));
-        Context.context.restore();
-    };
+    // // Rotated draw
+    // this.rot = function(x, y, angle) {
+    //     Context.context.save();
+    //     Context.context.translate(x, y);
+    //     Context.context.rotate(angle * this.TO_RADIANS);
+    //     Context.context.drawImage(this.image, -(this.image.width/2), -(this.image.height/2));
+    //     Context.context.restore();
+    // };
 };
