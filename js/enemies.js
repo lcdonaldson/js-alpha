b370e7f800ecf65faff569f0f511e953d5d9b032
Levi Donaldson <lcdbacanas@gmail.com>
2018-05-17 20:23:55 -0500

    Update enemies.js

--- js/enemies.js
+++ js/enemies.js
@@ -1,1 +1,46 @@
 
+    // End Level Bosses
+
+// ash boss
+const Arandul = {
+
+}
+
+// mountain boss
+const Goolgoth = {
+
+}
+
+// fire boss
+const Fogonan = {
+
+}
+
+// swamp boss
+const Aquasol = {
+  
+}
+
+// famine boss
+const Desariat = {
+
+}
+
+// dirt boss
+const Teradune = {
+
+}
+
+    // Stage Bosses
+
+const Sparkaoth = {}
+
+const Riordan = {}
+
+const Osso = {}
+
+const Loam = {}
+
+    // Minions
+
+
